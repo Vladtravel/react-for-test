48136843ba91e6389a040e2abc148c9623946e17
Vladyslav <vladtravel2015@gmail.com>
2021-10-22 16:22:30 +0300

    make it better

--- src/App.js
+++ src/App.js
@@ -1,13 +1,47 @@
+import { Switch } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { useEffect } from "react";
+import Container from "./components/Container/Container";
+import HomeView from "./components/HomeView/HomeView";
+import AppBar from "./components/AppBar/AppBar";
+import RegisterForm from "./components/RegisterForm/RegisterForm";
 import LoginForm from "./components/LoginForm/LoginForm";
-import s from "./App.module.css";
-import Navigation from "../src/components/Navigation/Navigation";
+import ContactsView from "./components/ContactsView/ContactsView";
+import PrivateRoute from "./components/PrivateRoute";
+import PublicRoute from "./components/PublicRoute";
+import operations from "./redux/operations";
+import "./App.module.css";
 
 function App() {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(operations.getCurrentUser());
+    //eslint-disable-next-line
+  }, []);
   return (
-    <div className={s.App}>
-      {/* <Navigation /> */}
-      <LoginForm />
-    </div>
+    <Container>
+      <AppBar />
+      <Switch>
+        <PublicRoute path="/" exact component={HomeView} />
+        <PublicRoute
+          path="/register"
+          component={RegisterForm}
+          restricted
+          redirectTo="/contacts"
+        />
+        <PublicRoute
+          path="/login"
+          component={LoginForm}
+          restricted
+          redirectTo="/contacts"
+        />
+        <PrivateRoute
+          path="/contacts"
+          component={ContactsView}
+          redirectTo="/login"
+        />
+      </Switch>
+    </Container>
   );
 }
 
--- src/components/LoginForm/LoginForm.js
+++ src/components/LoginForm/LoginForm.js
@@ -1,26 +1,12 @@
 import { useState } from "react";
-// import { useDispatch } from "react-redux";
-// import operations from "../../redux/operations";
+import { useDispatch } from "react-redux";
+import operations from "../../redux/operations";
 import s from "./LoginForm.module.css";
-
-// import TextField from "@material-ui/core/TextField";
-// import Button from "@material-ui/core/Button";
-// import { makeStyles } from "@material-ui/core/styles";
-
-// const useStyles = makeStyles({
-//   textField: {
-//     marginBottom: "15px",
-//   },
-//   button: {
-//     display: "block",
-//     letterSpacing: "1px",
-//   },
-// });
 
 function LoginForm() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  //   const dispatch = useDispatch();
+  const dispatch = useDispatch();
 
   const handleChange = (e) => {
     switch (e.currentTarget.name) {
@@ -40,83 +26,56 @@ function LoginForm() {
       alert("Fill all fields!");
       return;
     }
-    //   dispatch(operations.logIn({ email, password }));
+    dispatch(operations.logIn({ email, password }));
     setEmail("");
     setPassword("");
   };
 
-  // const classes = useStyles();
-
   return (
     <>
-      <div className={s.loginForm}>
-        <div className={s.loginForm__header}>
-          <h1 className={s.loginForm__title}>Login in to your account</h1>
+      <div className={s.frontPage}>
+        <div className={s.loginForm}>
+          <div className={s.loginForm__header}>
+            <h1 className={s.loginForm__title}>Login in to your account</h1>
+          </div>
+          <div className={s.loginFormBody}>
+            <form onSubmit={handleSubmit}>
+              <div className={s.EmailForm}>
+                <label className={s.loginFormBody__label}>
+                  Email address
+                  <div className={s.inputIconEmail}></div>
+                  <input
+                    className={s.loginFormBody__input}
+                    type="email"
+                    name="email"
+                    value={email}
+                    onChange={handleChange}
+                  />
+                </label>
+              </div>
+              <div className={s.PasswordForm}>
+                <label className={s.loginFormBody__label}>
+                  Password
+                  <div className={s.inputIconPassword}></div>
+                  <input
+                    className={s.loginFormBody__input}
+                    type="password"
+                    name="password"
+                    value={password}
+                    onChange={handleChange}
+                  />
+                </label>
+              </div>
+              <button className={s.loginFormBody__button} type="submit">
+                Sign In
+              </button>
+            </form>
+          </div>
+          <a className={s.loginFormFooter} href="">
+            Create an account
+          </a>
         </div>
-        <div className={s.loginFormBody}>
-          <form onSubmit={handleSubmit}>
-            <div className={s.EmailForm}>
-              <label className={s.loginFormBody__label}>
-                Email address
-                <div className={s.inputIconEmail}></div>
-                <input
-                  className={s.loginFormBody__input}
-                  type="email"
-                  name="email"
-                  value={email}
-                  onChange={handleChange}
-                />
-              </label>
-            </div>
-            <div className={s.PasswordForm}>
-              <label className={s.loginFormBody__label}>
-                Password
-                <div className={s.inputIconPassword}></div>
-                <input
-                  className={s.loginFormBody__input}
-                  type="password"
-                  name="password"
-                  value={password}
-                  onChange={handleChange}
-                />
-              </label>
-            </div>
-            <button className={s.loginFormBody__button} type="submit">
-              Sign In
-            </button>
-          </form>
-        </div>
-        <a className={s.loginFormFooter} href="">
-          Create an account
-        </a>
       </div>
-      {/* <form onSubmit={handleSubmit} className={s.logForm} autoComplete="off">
-        <h1 className={s.logTitle}>Рабочий стол</h1>
-        <TextField
-          type="email"
-          name="email"
-          value={email}
-          onChange={handleChange}
-          className={classes.textField}
-          size="small"
-          label="E-mail"
-          variant="outlined"
-        />
-
-        <TextField
-          type="password"
-          name="password"
-          value={password}
-          onChange={handleChange}
-          className={classes.textField}
-          size="small"
-          label="Password"
-          variant="outlined"
-        />
-        <Button className={classes.button} size="small" variant="contained" color="primary" type="submit">
-          Вход
-        </Button>
-      </form> */}
     </>
   );
 }
