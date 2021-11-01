249fd0d58ac69f459abe0ee14ad482a17ca0691b
Vladyslav <vladtravel2015@gmail.com>
2021-11-01 15:31:11 +0200

    make it better

--- src/App.js
+++ src/App.js
@@ -27,7 +27,7 @@ function App() {
           path="/signup"
           component={RegisterForm}
           restricted
-          redirectTo="/contacts"
+          redirectTo="/login"
         />
         <PublicRoute
           path="/login"
