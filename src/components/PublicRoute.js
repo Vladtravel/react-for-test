b80ab10d9bd8a3638e5cb5a316961df915904284
Vladyslav <vladtravel2015@gmail.com>
2021-11-01 17:42:33 +0200

    make it better

--- src/components/PublicRoute.js
+++ src/components/PublicRoute.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Route, Redirect } from "react-router-dom";
 import { useSelector } from "react-redux";
 import selectors from "../redux/selectors";
+import LoginForm from "../components/LoginForm/LoginForm";
 
 /**
  * - Если маршрут ограниченный, и пользователь залогинен, рендерит редирект на /contacts
@@ -16,7 +17,8 @@ const PublicRoute = ({ component: Component, redirectTo, ...routeProps }) => {
         isAuthenticated && routeProps.restricted ? (
           <Redirect to={redirectTo} />
         ) : (
-          <Component {...props} />
+          <LoginForm />
+          // <Component {...props} />
         )
       }
     />
