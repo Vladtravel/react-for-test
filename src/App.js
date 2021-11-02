f434ae0b5dec620d69af93aaa3dbeec355f8292a
Vladyslav <vladtravel2015@gmail.com>
2021-11-02 17:22:53 +0200

    make it better

--- src/App.js
+++ src/App.js
@@ -38,7 +38,7 @@ function App() {
           path="/login"
           component={LoginForm}
           restricted
-          redirectTo="/contacts"
+          redirectTo="/confirmation"
         />
 
         <PrivateRoute
